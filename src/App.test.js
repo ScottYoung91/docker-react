21f5b765b302c01340f5f9fb7e1e51a180d14faa
Scott Young <sbyoung25@gmail.com>
2022-07-08 15:58:43 -0500

    travis

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders react', () => {
+
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/react/);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
